513a061f245d233d743ace03a5a7ef7230704347
LDrago1802 <96987910+LDrago1802@users.noreply.github.com>
2022-01-13 20:18:33 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*  #include means add the folowing library while compiling as C is an old language hence,
+    it doesn't come with many funtions out of the box, so people before me has already made some 'advanced' functions
+    And I am just telling the complier to use them when compiling this program */
+
+int main(void)
+
+/*  As c translates to machine language and talks to computer in 0s and 1s so it obviously needs an 'int' to define the code.
+    While main contains our code to run
+    void is just there to tell the computer run the code but it's not an input(I will clear this in upcoming classes) */
+
+{
+    printf("Namaste World\n");
+
+    // print - understood, f = format as described in '()'
+    // \n is to break line so $ appears in next line in terminal
+}
+
+// {} are to contain our code
